7cc8f6952e916e04834afb2f5b0cfaaa9319b9bb
Sloththeswift <71036969+Sloththeswift@users.noreply.github.com>
2021-12-14 12:23:36 +0100

    Added separate menus for id and titlesearch.

--- FindaFlick/Menus/Idmenu.cs
+++ FindaFlick/Menus/Idmenu.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Console;
 
 namespace FindaFlick
 {
-    class Mainmenu
+    class Idmenu
     {
-        public static void RunMainMenu()
+        public static void RunIdMenu()
         {
             string icon = @" ___                  _         ___   _                   
 (  _`\  _            ( )       (  _`\(_ )  _        ( )    
@@ -19,16 +18,30 @@ namespace FindaFlick
 (_)    (_)(_) (_)`\__,_)`\__,_)(_)   (___)(_)`\____)(_) (_)
                                                            
                                                            
-Welcome to FindaFlick, let's find some movie trivia for you!
-Use the arrow keys and enter to navigate the app.
+Here you can search for a movie by ID.
 
 
 ";
-            
-            string[] upDown= { "Search movie by name", "Search movie by ID", "Exit app" };
+
+            string[] upDown = { "Search movie by ID", "Return to mainmenu", "Exit app" };
             MenuSkeleton menu = new MenuSkeleton(icon, upDown);
             int HighIndex = menu.Nav();
-            
+            switch (HighIndex)
+            {
+                case 0:
+                    Titlemenu.RunTitleMenu();
+                    break;
+
+                case 1:
+                    Mainmenu.RunMainMenu();
+                    break;
+
+                case 2:
+                    Environment.Exit(0);
+                    break;
+
+            }
+
 
         }
     }
